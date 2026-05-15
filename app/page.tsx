b9ce0276585466b27560dee0379b73aded51f053
dhairya2009 <dhairyaagrawal199@gmail.com>
2026-05-16 00:29:12 +0530

    Comments Removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-// import Searchbar from "./components/common/searchbar";
 
 function page() {
   return (
     <div className="text-[#F5F7FA] bg-[#080b11] h-227.5">
-      {/* <Searchbar /> */}
       Hello
     </div>
   );
